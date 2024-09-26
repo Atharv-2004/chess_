a9822d6383b67c8f7e951ec5da1c7bac1c57f317
Atharv Sanjay Jain <atharv.23bcs10113@ms.sst.scaler.com>
2024-09-26 20:28:50 +0530

    knight's movement is implemented

--- public/javascript/chess/pieces/knight.js
+++ public/javascript/chess/pieces/knight.js
@@ -6,6 +6,34 @@ var Knight = function(config){
 
 
 Knight.prototype = new Piece({});
-Knight.prototype.move = function(newPosition){
+
+Knight.prototype.isValidPosition= function(targetPosition){
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+
+    let targetCol = targetPosition.col;
+    let targetRow = parseInt(targetPosition.row);
+
+    // Check if the move is valid
+    if (Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0)) === 2 &&
+        Math.abs(targetRow - currentRow) === 1) {
+        return true;
+    } else if (Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0)) === 1 &&
+               Math.abs(targetRow - currentRow) === 2) {
+        return true;
+    }
+    return false; 
+};
+
+
+
+Knight.prototype.moveTo = function(targetPosition){
+    if(this.isValidPosition(targetPosition)){
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();
+    }else{
+        //NOOP
+        console.log("Invalid Move!!!");
+    }
 
 }
